6b65d506e809b54e930033bb393d9ddbbe1700af
Hisham Jaffar <hishamjaffar98@gmail.com>
2021-10-28 16:58:16 -0600

    Added place name function

---
+++ Graffiti-Proto/Assets/TextInputHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextInputHandler : MonoBehaviour
+{
+    [SerializeField] GameObject[] inputFields;
+    [SerializeField] TextMeshProUGUI[] nameFields;
+    void Start()
+    {
+        
+    }
+
+    public void PlaceNames()
+    {
+        for(int i=0;i<inputFields.Length;i++)
+        {
+            nameFields[i].text=inputFields[i].transform.GetChild(0).transform.GetChild(2).GetComponent<TextMeshProUGUI>().text;
+            inputFields[i].SetActive(false);
+        }
+    }
+}

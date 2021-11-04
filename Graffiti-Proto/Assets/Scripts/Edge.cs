6a0e531d74302e41fd843c8ecc16e507ab10450e
Hans Gunther <hnsgunther@gmail.com>
2021-11-04 10:00:15 -0600

    Fixed a bug where edges rotations were flipped

--- Graffiti-Proto/Assets/Scripts/Edge.cs
+++ Graffiti-Proto/Assets/Scripts/Edge.cs
@@ -22,7 +22,7 @@ public class Edge : MonoBehaviour
 
         // Rotate to correct angle
         Vector3 dif = nodes[1].transform.position - nodes[0].transform.position;
-        float angle_degrees = Vector3.Angle(dif, Vector3.right);
+        float angle_degrees = Vector3.Angle(dif, Vector3.left);
         transform.Rotate(new Vector3(0, 0, angle_degrees));
 
         // Scale to match distance

d39c876f06c6646e3e45e18113daf372af53bf5d
hgunther <hnsgunther@gmail.com>
2021-11-08 23:58:55 -0700

    Added a test button that enables and disables as nodes are selected

---
+++ Graffiti-Proto/Assets/Scripts/TestButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestButton : MonoBehaviour
+{
+    public NodeManager nodeManager;
+    bool interactable = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (nodeManager.selectedNode != null && !interactable)
+        {
+            AllowInteractions();
+        }
+        else if (nodeManager.selectedNode == null && interactable)
+        {
+            DisallowInteractions();
+        }
+    }
+
+    void AllowInteractions()
+    {
+        GetComponent<Button>().interactable = true;
+        interactable = true;
+    }
+
+    void DisallowInteractions()
+    {
+        GetComponent<Button>().interactable = false;
+        interactable = false;
+    }
+}

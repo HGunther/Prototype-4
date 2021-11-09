8a4cf19453451e61d803391fdead76a2accba608
Hisham Jaffar <hishamjaffar98@gmail.com>
2021-11-09 03:45:58 -0700

    Changed node sprites

---
+++ Graffiti-Proto/Assets/Scripts/NodeDisplay.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NodeDisplay : MonoBehaviour
+{
+    public NodeManager nodeManager;
+    Node displayingNode;
+    bool enabled = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (nodeManager.selectedNode == null && enabled)
+        {
+            Disable();
+        }
+        if (nodeManager.selectedNode != null && nodeManager.selectedNode != displayingNode)
+        {
+            DisplayNode();
+        }
+    }
+
+    void Enable()
+    {
+        GetComponent<Image>().enabled = true;
+        var children = GetComponentsInChildren<Text>();
+        foreach (Text text in children)
+        {
+            text.enabled = true;
+        }
+
+        enabled = true;
+    }
+
+    void Disable()
+    {
+        GetComponent<Image>().enabled = false;
+        var children = GetComponentsInChildren<Text>();
+        foreach (Text text in children)
+        {
+            text.enabled = false;
+        }
+
+        enabled = false;
+    }
+
+    void DisplayNode()
+    {
+        Enable();
+        var children = gameObject.GetComponentsInChildren<Text>();
+        foreach (var text in children)
+        {
+            if (text.gameObject.name == "Name")
+            {
+                text.text = nodeManager.selectedNode.gameObject.name;
+            }
+            else if (text.gameObject.name == "Date")
+            {
+                var size = nodeManager.selectedNode.testResults.Count;
+                if (size == 0)
+                {
+                    text.text = "None";
+                }
+                else
+                {
+                    text.text = "Day " + nodeManager.selectedNode.testResults[size - 1].testDate.ToString();
+                }
+            }
+            else if (text.gameObject.name == "Result")
+            {
+                var size = nodeManager.selectedNode.testResults.Count;
+                if (size == 0)
+                {
+                    text.text = "";
+                }
+                else
+                {
+                    var result = nodeManager.selectedNode.testResults[size - 1].testResult;
+                    if (result)
+                    {
+                        text.text = "Positive";
+                    }
+                    else
+                    {
+                        text.text = "Negative";
+                    }
+                }
+            }
+        }
+    }
+}
--- Graffiti-Proto/Assets/Scripts/NodeManager.cs
+++ Graffiti-Proto/Assets/Scripts/NodeManager.cs
@@ -6,6 +6,8 @@ public class NodeManager : MonoBehaviour
 {
     GameObject[] nodes;
     GameObject[] edges;
+
+    public Node selectedNode;
     void Start()
     {
         nodes = GameObject.FindGameObjectsWithTag("Node");
@@ -15,7 +17,64 @@ public class NodeManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            var mousePos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            var hits = Physics2D.GetRayIntersectionAll(ray);
+            bool background = false;
+            bool foundNode = false;
+            foreach (var hit in hits)
+            {
+                var gameObject = hit.transform.gameObject;
+                if (gameObject.tag == "Background")
+                {
+                    background = true;
+                }
+                else
+                {
+                    var node = gameObject.GetComponent<Node>();
+                    if (node != null)
+                    {
+                        foundNode = true;
+                        SelectNode(node);
+                    }
+                }
+            }
+            if (!foundNode && background)
+            {
+                DeselectNode();
+            }
+        }
+    }
+
+    public void SelectNode(Node node)
+    {
+        if (node == selectedNode)
+        { return; }
+
+        // deselect previous
+        if (selectedNode != null)
+        {
+            selectedNode.isSelected = false;
+            selectedNode.OnUnselected();
+        }
+
+        // select new
+        node.isSelected = true;
+        node.OnSelected();
+
+        selectedNode = node;
+    }
+
+    public void DeselectNode()
+    {
+        if (selectedNode != null)
+        {
+            selectedNode.isSelected = false;
+            selectedNode.OnUnselected();
+        }
+        selectedNode = null;
     }
 
     public void OnNewDay(){
